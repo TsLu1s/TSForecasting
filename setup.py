f8134f56ea388e56b2623fe2ce86e86b12351b8e
Luís Santos <80392073+TsLu1s@users.noreply.github.com>
2023-03-01 16:50:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="tsforecasting",
-    version="1.2.40",
+    version="1.2.45",
     description="TSForecasting is an automated time series forecasting framework",
     long_description=long_description,      
     long_description_content_type="text/markdown",
