c4c4e06190e3017239d36534682df50c1bb2287d
Luís Santos <80392073+TsLu1s@users.noreply.github.com>
2023-05-03 18:07:19 +0100

    Update simple_approach.py

--- examples/simple_approach.py
+++ examples/simple_approach.py
@@ -21,7 +21,7 @@ train_size_=0.9
 forecast_size_=10
 window_size_=10
 granularity_='1mo' # 1m,30m,1h,1d,1wk,1mo
-list_models_=['RandomForest', 'ExtraTrees', 'GBR', 'KNN', 'XGBoost','AutoArima'] #,'H2O_AutoML'] 
+list_models_=['RandomForest', 'ExtraTrees', 'GBR', 'KNN', 'XGBoost','AutoArima'] #,'Prophet','H2O_AutoML'] 
 eval_metric_="MAE" # MAPE, MSE
 
 
